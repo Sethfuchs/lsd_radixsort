50794393386b555cbc546d4ae52a8c5468680b42
Seth Fuchs <sethfuchs100@gmail.com>
2017-12-13 00:41:04 +0900

    update tag info

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'lsd_radixsort',
   packages = ['lsd_radixsort'],
-  version = '0.2',
+  version = '0.2.2',
   description = 'Implements the least signficant digit version of radix sort, and has the ability to sort negative and positive integers in an array',
   author = 'Seth Michael Fuchs',
   author_email = 'seth@minerva.kgi.edu',
   url = 'https://github.com/Sethfuchs/lsd_radixsort', # use the URL to the github repo
-  download_url = 'https://github.com/Sethfuchs/lsd_radixsort/archive/0.2.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/Sethfuchs/lsd_radixsort/archive/0.2.2.tar.gz', # I'll explain this in a second
   keywords = ['sorting', 'radix', 'lsd radix'], 
   classifiers = [],
 )
